8fad6ffc9480d0e579566a5cbca468d133b5d19b
Katsuaki Tabei <ta19760730ka@gmail.com>
2020-04-11 19:58:15 +0900

    no message

--- src/AutoCrossPairs.ts
+++ src/AutoCrossPairs.ts
@@ -1,9 +1,28 @@
-//////////////////////
-// 自動ペア組みプログラム
-//
+//////////////////////////////////////////
+// ファイルを開いたときのイベントハンドラ
 // 作成者：K.TABEI
 // 作成日：2020年3月17日
-//////////////////////
+//////////////////////////////////////////
+const onOpen = (): void => {
+  const ui: GoogleAppsScript.Base.Ui = SpreadsheetApp.getUi();
+  const menu: GoogleAppsScript.Base.Menu = ui.createMenu('スクリプト');
+  menu.addItem('自動ペア組み', 'onAutoCrossPairs');
+  menu.addToUi();
+}
+
+/**
+ * メニューから「自動ペア組み」が選択された場合の処理
+ */
+const onAutoCrossPairs = (): void => {
+  //TBScript.AutoCrossPairs();
+  AutoCrossPairs();
+}
+
+//////////////////////////////////////////
+// 自動ペア組みプログラム
+// 作成者：K.TABEI
+// 作成日：2020年3月17日
+//////////////////////////////////////////
 const AutoCrossPairs = (): void => {
 
   // 【定数】生徒名が登録されているシートの列番号
